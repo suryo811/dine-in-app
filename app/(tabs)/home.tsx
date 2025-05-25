abc63b071e178557ee15c5dea01264d8a28e7000
suryoday <suryodaykaushik@gmail.com>
2025-05-25 14:36:19 +0530

    implement restaurant listing feature on home screen with FlatList, add restaurant data to store, and enhance UI with special offers section

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,37 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { restaurants } from "../../store/restaurants";
+
 export default function Home() {
+  const renderItem = ({ item }: any) => {
+    return (
+      <TouchableOpacity
+        className="bg-[#5f5f5f] w-70 h-70 p-4 rounded-lg m-2"
+        style={{ elevation: 6 }}
+      >
+        <Image source={{ uri: item.image }} className="w-full h-40 rounded-lg" resizeMode="cover" />
+        <Text className="text-white text-xl font-bold mt-2 truncate">{item.name}</Text>
+        <Text className="text-white text-sm mt-1 truncate">{item.address}</Text>
+        <Text className="text-white text-sm mt-1">
+          {item.opening} - {item.closing}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <SafeAreaView style={{ backgroundColor: "#2b2b2b" }}>
       <View className="items-center">
-        <View className="bg-[#5f5f5f] w-11/12 rounded-lg p-4 shadow-lg">
+        <View className="bg-[#5f5f5f] w-11/12 rounded-lg p-4" style={{ elevation: 6 }}>
           <View className="flex-row items-center justify-between">
             <View className="flex-row items-center">
               <Text className="text-white text-2xl font-bold">Welcome to </Text>
@@ -22,12 +48,46 @@ export default function Home() {
           </View>
         </View>
       </View>
-      <ScrollView>
+      <ScrollView stickyHeaderIndices={[0]}>
         <ImageBackground
           resizeMode="cover"
           source={require("../../assets/images/homeBanner.png")}
-          className="h-52 my-4 w-full items-center justify-center"
-        ></ImageBackground>
+          className="h-52 mt-1 mb-4 w-full bg-[#2b2b2b] items-center justify-center"
+        />
+
+        <View className="px-4 py-2 items-start ">
+          <Text className="text-white text-3xl font-bold">Special Offers</Text>
+        </View>
+
+        {restaurants.length > 0 ? (
+          <FlatList
+            data={restaurants}
+            horizontal
+            keyExtractor={(item) => item.id.toString()}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 10 }}
+            renderItem={renderItem}
+          />
+        ) : (
+          <ActivityIndicator animating={true} size="large" color="#fb9b33" />
+        )}
+
+        <View className="px-4 py-2 items-start mt-4">
+          <Text className="text-[#fb9b33] text-3xl font-bold">Our Restaurants</Text>
+        </View>
+
+        {restaurants.length > 0 ? (
+          <FlatList
+            data={restaurants}
+            horizontal
+            keyExtractor={(item) => item.id.toString()}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 10, paddingBottom: 50 }}
+            renderItem={renderItem}
+          />
+        ) : (
+          <ActivityIndicator animating={true} size="large" color="#fb9b33" />
+        )}
       </ScrollView>
     </SafeAreaView>
   );
