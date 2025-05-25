2de4fbe6fbf3e87da9636d7c0fbbcd49da814083
suryoday <suryodaykaushik@gmail.com>
2025-05-25 11:42:09 +0530

    add expo-linear-gradient for enhanced UI in home screen, update package.json and package-lock.json, and include new images for branding

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,8 +1,34 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: "#2b2b2b" }}>
+      <View className="items-center">
+        <View className="bg-[#5f5f5f] w-11/12 rounded-lg p-4 shadow-lg">
+          <View className="flex-row items-center justify-between">
+            <View className="flex-row items-center">
+              <Text className="text-white text-2xl font-bold">Welcome to </Text>
+              <LinearGradient
+                colors={["#FF5F6D", "#FFC371"]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 0 }}
+                style={{ borderRadius: 5, padding: 3 }}
+              >
+                <Text className="text-2xl font-bold">Dine Time</Text>
+              </LinearGradient>
+            </View>
+            <Image className="w-10 h-10" source={require("../../assets/images/icon.png")} />
+          </View>
+        </View>
+      </View>
+      <ScrollView>
+        <ImageBackground
+          resizeMode="cover"
+          source={require("../../assets/images/homeBanner.png")}
+          className="h-52 my-4 w-full items-center justify-center"
+        ></ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
